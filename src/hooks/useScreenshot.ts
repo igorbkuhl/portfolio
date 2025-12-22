28af3e97da8ef55af499d135095f1ccadcf33709
igorbkuhl <igorb.kuhl@gmail.com>
2025-12-22 17:38:29 -0300

    fix: adapt screenshot function to convert lab colors to hex due to html2canvas limitations

--- src/hooks/useScreenshot.ts
+++ src/hooks/useScreenshot.ts
@@ -1,17 +1,55 @@
+import Color from "color";
 import html2canvas from "html2canvas";
 import { useState, useEffect } from "react";
 
 export function useScreenshot() {
-    const [base64, setBase64] = useState<string>("");
+  const [base64, setBase64] = useState<string>("");
 
-    useEffect(() => {
-        const screenshotTarget = document.getElementById("main-content")!;
+  useEffect(() => {
+    const screenshotTarget = document.getElementById("main-content")!;
 
-        html2canvas(screenshotTarget).then((canvas) => {
-            const base64Image = canvas.toDataURL("image/png");
-            setBase64(base64Image);
-        })
-    }, []);
+    /**
+     * Recursively converts any CSS color property from lab to hex.
+     * This action is required for html2canvas due to unsupported color space from tailwindcss v4.
+     * @param elements HTMLCollection
+     */
+    const convertColors = (elements: HTMLCollection) => {
+      if (!elements.length) return;
 
-    return base64;
+      Array.from(elements).forEach(element => {
+        const computedStyles = window.getComputedStyle(element);
+
+        Object.values(computedStyles).forEach(key => {
+          const value = computedStyles.getPropertyValue(key);
+
+          if (value.startsWith("lab(")) {
+            const labValues = value
+              .replace(/\D+/, "")
+              .split(" ")
+              .map(v => parseFloat(v));
+            screenshotTarget.style.setProperty(
+              key, Color.lab(...labValues).hex()
+            );
+          }
+        });
+
+        convertColors(element.children);
+      });
+    }
+
+    /**
+     * Captures a screenshot of the page's top content.
+     */
+    const screenshot = () => {
+      html2canvas(screenshotTarget).then((canvas) => {
+        const base64Image = canvas.toDataURL("image/png");
+        setBase64(base64Image);
+      }).catch(err => console.error("Screenshot error: ", err));
+    }
+
+    convertColors(screenshotTarget.children);
+    screenshot();
+  }, []);
+
+  return base64;
 }
